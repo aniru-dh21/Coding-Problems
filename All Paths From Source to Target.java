a908ea78abae8cc39a3156146b666990fad461c5
Ramachandra Anirudh Vemulapalli <75429046+aniru-dh21@users.noreply.github.com>
2023-10-24 18:45:28 +0530

    Create All Paths From Source to Target.java

---
+++ All Paths From Source to Target.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(graph, 0, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private void dfs(int[][] graph, int v, List<List<Integer>> ans, List<Integer> path) {
+        path.add(v);
+        if(v == graph.length-1) {
+            ans.add(new ArrayList<>(path));
+        } else {
+            for(int i=0; i<graph[v].length; i++) {
+                dfs(graph, graph[v][i], ans, path);
+            }
+        }
+        path.remove(path.size()-1);
+    }
+}
